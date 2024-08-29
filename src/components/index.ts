a2e05e7a4a58594110b84ac98649444780ef4624
Aman Agarwal <96075278+codeamanwal@users.noreply.github.com>
2024-08-30 02:09:26 +0530

    Add files via upload

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+
+export { 
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+};
+export * from './tags/user-tag';
+export * from './text';
+export * from './accordion';
+export * from "./tasks/form/description";
+export * from "./tasks/form/due-date";
+export * from "./tasks/form/stage";
+export * from "./tasks/form/title";
+export * from "./tasks/form/users";
+export * from "./tasks/form/header";
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import List from "../tasks/list"
+
+export const Home = () => {
+
+  return (
+    <div>
+      <List/>
+    </div>
+  )
+}
